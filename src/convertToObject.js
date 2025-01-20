f41128c10d48461944a2963c272e537e5a257939
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2025-01-20 11:45:08 +0200

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const lines = sourceString.split(';');
+  const result = {};
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+
+    if (line.includes(':')) {
+      const parts = line.split(':');
+      const key = parts[0].trim();
+      const value = parts.slice(1).join(',\n').trim();
+
+      result[key] = value;
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
